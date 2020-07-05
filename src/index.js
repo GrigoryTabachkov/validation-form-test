9daed73f50867a254e14789182dd142c5b00d3d5
GrigoryTabachkov <tabbartend@gmail.com>
2020-07-05 15:57:20 +0300

    error handler addedd & alerts removed + minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById('main-form');
 const userName = document.getElementById('username');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
@@ -18,6 +18,20 @@ function validPhone(phone) {
   return phoneRegEx.test(phone);
 }
 
+function errorHandler(input, message) {
+  const form = input.parentElement;
+  const mistake = form.querySelector('small');
+  form.className = 'form error';
+  mistake.innerText = message;
+}
+
+function successHandler(input) {
+  const form = input.parentElement;
+  form.className = 'form success';
+  // const errorCleaner = document.querySelector('small');
+  // errorCleaner.remove();
+}
+
 function validator(event) {
   event.preventDefault();
 
@@ -26,22 +40,27 @@ function validator(event) {
   const phoneInput = phone.value.trim();
 
   if (userNameInput === '') {
-    alert('Заполните ФИО полностью');
+    errorHandler(userName, 'Заполните ФИО полностью');
   } else if (!validUserName(userNameInput)) {
-    alert('Введите ФИО в формате Петров Иван Васильевич');
+    errorHandler(userName, 'Введите ФИО в формате Петров Иван Васильевич');
+  } else {
+    successHandler(userName);
   }
 
   if (emailInput === '') {
-    alert('Для регистрации необходим email');
+    errorHandler(email, 'Для регистрации необходим email');
   } else if (!validEmail(emailInput)) {
-    alert('Email должен принадлежать домену gmail.com');
+    errorHandler(email, 'Email должен принадлежать домену gmail.com и не содержать символов, кроме латинских букв и цифр');
+  } else {
+    successHandler(email);
   }
 
   if (phoneInput === '') {
-    alert('Введите Ваш номер мобильного телефона');
+    errorHandler(phone, 'Введите Ваш номер мобильного телефона');
   } else if (!validPhone(phoneInput)) {
-    alert(`Номер должен начинаться с +7*, 8*, или 07*
-    \nи содержать 11 цифр, вы ввели ${phoneInput.length} цифр`);
+    errorHandler(phone, 'Номер должен начинаться с +7*, 8*, или 07*');
+  } else {
+    successHandler(phone);
   }
 }
 
@@ -55,14 +74,15 @@ function pasteHandler(event) {
 
   switch (target) {
     case userName:
-      // tEsT=1234567890Иван`~!#$%^&*(Петрович)_+[{}]\|/Иванов:;"'<,>.?
+      // tEsT=1234567890Григорий'~!#$%Бо^ри&со*вич(Т)а_+ба[{ч}к]\о|/:;"'<в,>.?
       console.log(`\nВставка \n${pastedData} \nв поле c id="${target.id}"`);
-      target.value = pastedData.replace(/[^ А-яё]/g, ' ').replace(/\s+/g, ' ').trim();
+      // Корректировка выражения, чтобы избежать слишком длинных пробелов при вставке
+      target.value = pastedData.replace(/[^ А-яё]/g, '').trim();
       console.log(`Результат:\n${target.value}`);
       break;
 
     case email:
-      // taЫыbachkov`~!#$%^&*хаюкен()+[{}]\|:;"'<,>.?/grigЯory@gmail.com
+      // gb.Ыыtaba4kov`~!#$%^&*хаюкен()+[{}]\|:;"'<,>.?/@gmail.com
       console.log(`\nВставка \n${pastedData} \nв поле c id="${target.id}"`);
       target.value = pastedData.replace(/[^.\w.@gmail.com]/g, ' ').replace(/\s+/g, '').trim();
       console.log(`Результат:\n${target.value}`);
@@ -80,6 +100,7 @@ function pasteHandler(event) {
   }
 }
 
+// Думал использовать fetch, но решил остановиться на Promise.all
 async function newUser(event) {
   event.preventDefault();
 
@@ -97,6 +118,11 @@ async function newUser(event) {
     ]).catch((error) => console.log(error));
     console.log(userData);
   }
+
+  alert(`Поздравляем, вы наняты на работу в Nethouse!
+  \nФИО: ${userData.name}
+  \nEmail: ${userData.email}
+  \nТелефон: ${userData.phone}`);
 }
 
 form.addEventListener('paste', pasteHandler);
